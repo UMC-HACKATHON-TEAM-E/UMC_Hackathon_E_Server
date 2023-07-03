e03ba27169d20be417c8eb6ebf6558a56703b94b
Son <rooneei@naver.com>
2023-07-03 20:18:58 +0900

    [Feat]User

---
+++ src/main/java/com/example/demo/src/Dto/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.src.Dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+//@DynamicInsert
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String email;
+
+    @Column(nullable = false, length = 16)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 10)
+    private String name;
+}
