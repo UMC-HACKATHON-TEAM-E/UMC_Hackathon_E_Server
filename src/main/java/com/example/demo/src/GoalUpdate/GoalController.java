bcf3ba793707e1498569d8211843748490cd1e64
eojinny <96863137+eojinny@users.noreply.github.com>
2023-07-04 03:57:13 +0900

    Update GoalController.java

--- src/main/java/com/example/demo/src/GoalUpdate/GoalController.java
+++ src/main/java/com/example/demo/src/GoalUpdate/GoalController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/goal")
