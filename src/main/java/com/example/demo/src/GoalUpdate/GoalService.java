d332bb43509478be06baa3ed5d4bb7d9d1196f5b
eojinny <96863137+eojinny@users.noreply.github.com>
2023-07-04 03:52:58 +0900

    Update GoalService.java

--- src/main/java/com/example/demo/src/GoalUpdate/GoalService.java
+++ src/main/java/com/example/demo/src/GoalUpdate/GoalService.java
@@ -23,4 +23,38 @@ public class GoalService {
         goal.update();
         return goalRepository.save(goal);
     }
+      public Goal read(Long goalId){
+        Goal goal = goalRepository.findByGoalId(goalId);
+        return goal;
+    }
+
+    public Double progress(Long goalId) throws ParseException {
+        Goal goal = goalRepository.findByGoalId(goalId);
+        Date createGoal =goal.getStartDate();
+        LocalDate now = LocalDate.now();
+
+        Date format1 = new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(createGoal));
+        Date format2 = new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(now));
+        long diffSec = (format2.getTime() - format1.getTime()) / 1000; //초 차이
+        long diffDays = diffSec / (24*60*60);
+        //System.out.println(diffDays + "일 차이");
+        double x = goal.getCount() /(double) diffDays;
+        System.out.println(diffDays);
+        System.out.println(x);
+
+
+        Date format3 = new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(goal.getEndDate()));
+        Date format4 = new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(goal.getStartDate()));
+        long diffSe = (format3.getTime() - format4.getTime()) / 1000; //초 차이
+        long diffDay = diffSe / (24*60*60);
+        System.out.println(diffDay);
+        double y = goal.getGoalCount()/(double)diffDay;
+
+        System.out.println(y);
+
+        double ans =(x/y) * 100;
+
+        return ans;
+
+    }
 }
